8433e78496b4d09d330d277b1d4b3aba742ea8ff
Gabidemi <gabidemi@brooklynsteamcenter.org>
2023-03-27 10:51:38 -0400

    Posts in site

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
-
+import pymysql
+import pymysql.cursors
 
 app = Flask(__name__)
 
@@ -7,6 +8,30 @@ app = Flask(__name__)
 def index():
     print("Xcution_Testing_Site")
     return render_template("home.html.jinja")
+
+
+
+
+connection = pymysql.connect(
+    host="10.100.33.60",
+    user="gabidemi",
+    password="244655536",
+    database="gabidemi_Social",
+    cursorclass=pymysql.cursors.DictCursor,
+    autocommit=True
+)
+
+
+@app.route("/post")
+def post():
+    cursor=connection.cursor()
+
+    cursor.execute("SELECT * FROM `Posts` ORDER BY `date`")
+
+    results = cursor.fetchall()
+
+    return render_template("post.html.jinja", posts = results)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
